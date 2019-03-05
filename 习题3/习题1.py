c65c805ffa7daaaa5165a0a67db778a05e3c49da
sanyapeng <sanyapeng@playcrab.com>
2019-03-05 16:01:26 +0800

    xiti3

---
+++ 习题3/习题1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    File Name：       习题1
+#    Description :
+#    Author :          SanYapeng
+#    date：            2019-03-05
+#    Change Activity:  2019-03-05:
+
+#非整除
+print(7 / 4)
+
+# 整除
+print(7 // 4)
